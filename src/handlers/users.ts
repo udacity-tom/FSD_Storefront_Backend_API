83b4c2049d6f3fd3867f6b095cedf2a8b6f41df1
udacity-tom <udacity@iamtom.com>
2021-09-14 22:44:55 +0200

    Feat: setup user models/handlers for show, create, index

--- src/handlers/users.ts
+++ src/handlers/users.ts
@@ -1,1 +1,93 @@
-//users stub
+import express, { Request, Response } from 'express';
+import jwt, { Jwt } from 'jsonwebtoken';
+import { User, UserStore } from "../models/user";
+import { AuthStore } from "../middleware/auth";
+
+const userStore = new UserStore;
+const auth = new AuthStore;
+
+const index = async(req: Request, res: Response) => {
+    try {
+        const users = await userStore.index();
+        res.json(users);
+    } catch (err) {
+        res.status(400);
+        res.json(err);
+
+    }
+}
+
+
+const show = async(req: Request, res: Response) => {
+    try {
+        console.log('users.ts id', req.params.id);
+        const user = await userStore.show(req.params.id);
+        res.json(user);
+    } catch (err) {
+        res.status(400);
+        res.json(err);
+    }
+}
+
+// async authenticate(username: string, password: string): Promise<String> {
+//     try {
+//         const result = auth.authenticate(username, password)
+//         return result;
+//     } catch (err) {
+//         throw new Error('Unable to authenticate ${username}, error: ${err}');
+//     }
+// }
+
+const create = async(req: Request, res: Response) => {
+    try {
+        const user: User = {
+            username: req.body.username,
+            firstname: req.body.firstname,
+            lastname: req.body.lastname,
+            password: req.body.password
+        }
+        console.log('users.ts: user value', user);
+        
+        
+        const newUser = await userStore.create(user);
+        console.log('users.ts: newUser ', newUser);
+        res.send(newUser);
+
+
+    } catch (err) {
+        res.status(400);
+        res.json(err);
+    }
+}
+
+
+
+const authenticate = async (req: Request, res: Response) => {
+    try {
+        const username = req.body.username; 
+        const password = req.body.password;
+
+        // try {
+        //     jwt.verify(req.body.token, process.env.TOKEN_SECRET!);
+        // } catch (err) {
+        //     res.status(401);
+        //     res.send('Invalid Token ${err}');
+        //     return; //exit authentication prcess
+        // }
+        const userAuth = await auth.authenticate(username, password);
+        res.send(userAuth);
+
+    } catch (err) {
+        res.status(400);
+        res.send(err);
+    }
+}
+
+const userRoutes = (app: express.Application) => {
+    app.get('/users', index);
+    app.get('/users/:id', show);
+    app.post('/users/create', create);
+    app.post('/users/authenticate', authenticate);
+}
+
+export default userRoutes;
--- src/models/user.ts
+++ src/models/user.ts
@@ -1,1 +1,83 @@
-//user stub
+import { CONNREFUSED } from "dns";
+import client from "../database";
+import { AuthStore } from "../middleware/auth";
+
+const auth = new AuthStore;
+
+export type User = {
+    id?: number;
+    username: string;
+    firstname: string;
+    lastname: string;
+    password: string;
+};
+
+export class UserStore {
+    async index(): Promise<User[]> {
+        
+        try {
+            const conn = await client.connect();
+            const sql = 'SELECT * FROM users;';
+
+            const result = await conn.query(sql);
+            conn.release();
+            return result.rows;
+            
+        } catch (err) {
+            throw new Error(`Could not find any users. Error: ${err}`);
+        }
+    }
+
+    async create(user: User): Promise<User[]> {
+        try {
+            // console.log('user.ts: user ', user);
+            user.password = await auth.hashPassword(user.password);
+            // console.log('user.ts: user ', user);
+            const conn = await client.connect();
+            const sql = 'INSERT INTO users (username, firstname, lastname, password_hash) VALUES($1, $2, $3, $4) RETURNING *;';
+
+            const result = await conn.query(sql, [user.username, user.firstname, user.lastname, user.password]);
+            // console.log('user.ts: result', result);
+            conn.release();
+            return result.rows[0];
+
+            //take supplied user and store in database, on success return user in json form
+        } catch (err) {
+            throw new Error(`Something went wrong, try again. Error: ${err}`);
+        } 
+    }
+
+    async show(id: string): Promise<User> {
+        try {
+            console.log('user.ts: id is ', id);
+            const sql = 'SELECT * FROM users WHERE id=($1);';
+            const conn = await client.connect();
+            // const idNum = 
+            conn.release();
+            const result = await conn.query(sql, [id]);
+
+            return result.rows[0];
+        } catch (err) {
+            throw new Error(`There is no user with ID ${id}. Error: ${err}`);
+        } 
+    }
+
+
+    // async delete(): Promise<User[]> {
+    //     try {
+            
+    //     } catch (err) {
+            
+    //     } 
+    // }
+
+    // async authenticate(username: string, password: string): Promise<String> {
+    //     try {
+    //         const result = auth.authenticate(username, password)
+    //         return result;
+    //     } catch (err) {
+    //         throw new Error('Unable to authenticate ${username}, error: ${err}');
+    //     }
+    // }
+
+}
