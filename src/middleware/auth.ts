0bbb4e245cf22d3c43b7f4bc5c875652cee6e03f
udacity-tom <udacity@iamtom.com>
2021-09-16 10:07:53 +0200

    Feat: added verify jwt method

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,8 +1,8 @@
 import bcrypt from 'bcrypt';
 import dotenv from 'dotenv';
 import client from '../database';
-import jwt, { Jwt, JwtPayload } from 'jsonwebtoken';
-import express, { Request, Response } from 'express';
+import jwt from 'jsonwebtoken';
+import express from 'express';
 import { User } from '../models/user';
 
 //provides authentication functions, like: password encryption, JWT verification, etc.
@@ -15,20 +15,25 @@ const saltRounds = process.env.SALT_ROUNDS;
 export class AuthStore {
 async authenticate(username: string, password: string): Promise<String> {
     const conn = await client.connect();
-    const sql = 'SELECT password_hash FROM users WHERE username = ($1)';
+    const sql = 'SELECT * FROM users WHERE username = ($1)';
     const result = await conn.query(sql, [username]);
+    const user: User = result.rows[0];
     // console.log('auth.ts: passwordhash', result);
     if(result.rows.length) {
-        const userPassword = result.rows[0];
-        // console.log('auth.ts: userPassword', userPassword);
-        const passwordCheck = bcrypt.compareSync(password + pepper, userPassword.password_hash);
+        const userPassword = result.rows[0].password_hash;
+        console.log('auth.ts: userPassword', userPassword);
+        console.log('auth.ts: result.rows[0]', result.rows[0]);
+        const passwordCheck = bcrypt.compareSync(password + pepper, userPassword);
         // console.log('auth.ts: passwordCheck', passwordCheck);
         if (passwordCheck){
             console.log('auth.ts: bcrypt succeded');
-            return ('Success-login accepted');
+            user.password_hash = '';
+            user.lastname = '';
+            const token = this.createToken(user);
+            return (token);
         } else {
-            console.log('auth.ts: bcrypt failed');
-            return ('Success-login refused, try again');
+            // console.log('auth.ts: bcrypt failed');
+            return ('Failure-login refused, try again');
         }
     }
     return ('Unknown user, have you registered an account?');
@@ -42,15 +47,46 @@ async hashPassword(password: string): Promise<string> {
 // async authorise(req: Requsername: string): Promise<string> {
 async createToken(jwtPayloadData: User): Promise<string> {
     // const username: string = req.body.username;
-    console.log('auth.js: jwtPayloadData', jwtPayloadData);
+    console.log('auth.ts: jwtPayloadData', jwtPayloadData);
+    const options = {
+        expiresIn: '10h',
+        subject: 'access'
+    }
     try {
-    var token: string = jwt.sign(jwtPayloadData, process.env.TOKEN_SECRET!);
+    var token: string = jwt.sign(jwtPayloadData, process.env.TOKEN_SECRET!, options);
     } catch (err) {
         throw new Error(`Something went wrong. Err: ${err}`);
     }
     return token;
 }
 
+// async authorise(token: string): Promise<string> {
+async authorise(token: string): Promise<string> {
+    console.log('auth.ts: token', token);
+    try {
+        jwt.verify(token, process.env.TOKEN_SECRET!);
+    } catch(err) {
+        throw new Error(`Invalid Token!!`);
+    }
+    return 'valid';
+}
+
+async verifyAuthToken(
+    req: express.Request,
+    res: express.Response,
+    next: () => void
+  ) {
+    try {
+      const authorisationHeader = String(req.headers.authorization);
+      console.log('auth.ts: authorisationsHeader value: ', authorisationHeader);
+      const jwtToken: string = authorisationHeader?.split(' ')[1];
+      const decoded = jwt.verify(jwtToken, String(process.env.TOKEN_SECRET));
+      console.log('verifyJwt.ts: decoded value', decoded);
+      next();
+    } catch (err) {
+      res.status(401).json({message: 'Invalid Token!'});
+    }
+  }
 // async verifyJwt(jwtToken: string): Promise<string> {
 //      const validJwt: string | JwtPayload  = jwt.verify(jwtToken, process.env.TOKEN_SECRET!);
 //      if(validJwt) {
