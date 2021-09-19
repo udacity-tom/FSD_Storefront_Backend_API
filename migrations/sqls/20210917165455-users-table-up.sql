7733ce9704e18cee1055efb3f53d4db37d34718d
udacity-tom <udacity@iamtom.com>
2021-09-19 17:10:02 +0200

    Fix: db migrations typos, type declaration correction

--- migrations/sqls/20210917165455-users-table-up.sql
+++ migrations/sqls/20210917165455-users-table-up.sql
@@ -3,5 +3,6 @@ id SERIAL PRIMARY KEY,
 username VARCHAR(64) NOT NULL, 
 firstname VARCHAR(64) NOT NULL,
 lastname VARCHAR(64) NOT NULL,
-pasword TEXT) 
-UNIQUE(username);
+password TEXT,
+UNIQUE(username)
+);
--- migrations/sqls/20210917223741-order-products-table-up.sql
+++ migrations/sqls/20210917223741-order-products-table-up.sql
@@ -2,5 +2,5 @@ CREATE TABLE order_products(
 id SERIAL PRIMARY KEY, 
 product_id bigint REFERENCES products(id), 
 quantity integer,
-order_id bitint REFERENCES orders(id)
+order_id bigint REFERENCES orders(id)
 );
