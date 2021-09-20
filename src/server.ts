a4891d30f70daa457dd227c04d27bb70f8a554f2
udacity-tom <udacity@iamtom.com>
2021-09-20 14:15:50 +0200

    Fix: added export

--- src/server.ts
+++ src/server.ts
@@ -1,24 +1,24 @@
-import express, { Request, Response } from 'express'
-import bodyParser from 'body-parser'
+import express, { Request, Response } from 'express';
+import bodyParser from 'body-parser';
 import userRoutes from './handlers/users';
-
 
 const hostAddress = process.env.SERVER_ADDRESS || '127.0.0.1';
 const portAddress = process.env.SERVER_PORT || 3002;
 
-const app: express.Application = express()
-const address: string = `${hostAddress}:${portAddress}`;
+const app: express.Application = express();
+const address = `${hostAddress}:${portAddress}`;
 
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
-app.get('/', function (req: Request, res: Response) {
-    res.send('Hello World!')
-})
+app.get('/', function(req: Request, res: Response) {
+  res.send('Hello World!');
+});
 
 //handler routes
 userRoutes(app);
 
+app.listen(portAddress, function() {
+  console.log(`starting app on: ${address}`);
+});
 
-app.listen(portAddress, function () {
-    console.log(`starting app on: ${address}`)
-})
+export default app;
