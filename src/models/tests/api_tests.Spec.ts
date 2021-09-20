0549103a2f1719288a2b5bd5b3441a49e0d3f006
udacity-tom <udacity@iamtom.com>
2021-09-20 14:11:55 +0200

    Test: added basic initial tests for functionality test

--- src/models/tests/api_tests.Spec.ts
+++ src/models/tests/api_tests.Spec.ts
@@ -1,1 +1,41 @@
-//test stub
+import supertest from 'supertest';
+import app from '../../server';
+import { User, UserStore } from '../user';
+import { AuthStore } from '../../middleware/auth';
+import { response } from 'express';
+
+const store = new UserStore();
+const request = supertest(app);
+const token =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InRhbSIsImZpcnN0bmFtZSI6IlRpbW90aHkiLCJsYXN0bmFtZSI6IiIsInBhc3N3b3JkIjoiIiwiaWF0IjoxNjMyMTM1NDkyLCJleHAiOjE2MzQ3Mjc0OTIsInN1YiI6ImFjY2VzcyJ9.-5jpC3p1EgKFyv-0myrkI5UUOMFbJy7uPGLT0pfOD1g';
+const auth = new AuthStore();
+
+describe('Initial function availability tests->testing DB setup, DB, function existance, jasmine ', () => {
+  describe('User model/handler tests functions exists', () => {
+    it('checks the index() methods exists', async () => {
+      //   beforeAll(async () => {
+      const response = await request.get('/users');
+      //   console.log('response is: ', response);
+      expect(response.status).toBe(200);
+      //   });
+    });
+    it('checks method exists', () => {
+      expect(store.index).toBeDefined();
+    });
+
+    it('index method should return a list of users', async () => {
+      const result = await request
+        .get('/users')
+        .set('Authorization', 'Bearer ' + token);
+      //   console.log('result is ', result);
+      expect(result.status).toBe(200);
+    });
+  });
+  describe('Auth functions exist', () => {
+    it('checks auth.authenticate() function exists ', () => {
+      const username = 'Bill';
+      const password = 'password';
+      expect(auth.authenticate(username, password)).toBeDefined();
+    });
+  });
+});
--- src/models/tests/helpers/reporter.ts
+++ src/models/tests/helpers/reporter.ts
@@ -1,24 +1,22 @@
 import {
-    DisplayProcessor,
-    SpecReporter,
-    StacktraceOption
-  } from 'jasmine-spec-reporter';
-  import SuiteInfo = jasmine.SuiteInfo;
-  // import CustomReporter = jasmine.CustomReporter;
-  
-  class CustomProcessor extends DisplayProcessor {
-    public displayJasmineStarted(info: SuiteInfo, log: string): string {
-      return `TypeScript ${log}`;
-    }
+  DisplayProcessor,
+  SpecReporter,
+  StacktraceOption
+} from 'jasmine-spec-reporter';
+import CustomReporter = jasmine.CustomReporter;
+import SuiteInfo = jasmine.JasmineStartedInfo;
+class CustomProcessor extends DisplayProcessor {
+  public displayJasmineStarted(info: SuiteInfo, log: string): string {
+    return `TypeScript ${log}`;
   }
-  
-  jasmine.getEnv().clearReporters();
-  jasmine.getEnv().addReporter(
-    new SpecReporter({
-      spec: {
-        displayStacktrace: StacktraceOption.NONE
-      },
-      customProcessors: [CustomProcessor]
-    })
-  );
-  
+}
+
+jasmine.getEnv().clearReporters();
+jasmine.getEnv().addReporter(
+  (new SpecReporter({
+    spec: {
+      displayStacktrace: StacktraceOption.NONE
+    },
+    customProcessors: [CustomProcessor]
+  }) as unknown) as CustomReporter
+);
