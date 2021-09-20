4f0d9f9a64e7bc768c0494cbfce68284fbfa1bc6
udacity-tom <udacity@iamtom.com>
2021-09-20 19:19:40 +0200

    Feat: added models for products and orders

--- src/models/order.ts
+++ src/models/order.ts
@@ -1,1 +1,62 @@
-//Order stub
+import client from '../database';
+import { AuthStore } from '../middleware/auth';
+import { Request, Response } from 'express';
+
+const auth = new AuthStore();
+
+export type Order = {
+  id?: number;
+  user_id: number;
+  status: string;
+};
+
+export class OrderStore {
+  async index(): Promise<Order[]> {
+    try {
+      const sql = 'SELECT * FROM orders;';
+      const conn = await client.connect();
+      const result = await conn.query(sql);
+      conn.release();
+      return result.rows;
+    } catch (err) {
+      throw new Error(`There was an error finding products: ${err}`);
+    }
+  }
+
+  async show(id: string): Promise<Order> {
+    try {
+      const sql = 'SELECT * FROM orders WHERE id=($1);';
+      const conn = await client.connect();
+      const result = await conn.query(sql, [id]);
+      conn.release();
+      return result.rows[0];
+    } catch (err) {
+      throw new Error(`There was an error with ${id}. Erro: ${err}`);
+    }
+  }
+
+  async create(order: Order): Promise<Order> {
+    try {
+      const sql =
+        'INSERT INTO orders (user_id, status) VALUES ($1, $2) RETURNING *;';
+      const conn = await client.connect();
+      const result = await conn.query(sql, [order.user_id, order.status]);
+      conn.release();
+      return result.rows[0];
+    } catch (err) {
+      throw new Error(
+        `There was an error with order for ${order.user_id}. Error: ${err}`
+      );
+    }
+  }
+
+  //   async update(order: Order): Promise <Order> {
+  //       try {
+
+  //       } catch (err) {
+  //           throw new Error(`There was an error with updating the order: ${id}`)
+  //       }
+  //   }
+
+  //   async delete
+}
--- src/models/product.ts
+++ src/models/product.ts
@@ -1,1 +1,104 @@
-//product stub
+import client from '../database';
+import { AuthStore } from '../middleware/auth';
+import { Request, Response } from 'express';
+
+const auth = new AuthStore();
+
+export type Product = {
+  id?: number;
+  name: string;
+  price: number;
+  category: string;
+};
+
+export class ProductStore {
+  async index(): Promise<Product[]> {
+    try {
+      const sql = 'SELECT * FROM products;';
+      const conn = await client.connect();
+
+      const result = await conn.query(sql);
+      conn.release();
+      return result.rows;
+    } catch (err) {
+      throw new Error(`Could not find any products. Error: ${err}`);
+    }
+  }
+
+  async create(product: Product): Promise<Product> {
+    try {
+      const conn = await client.connect();
+
+      const sql =
+        'INSERT INTO products (name, price, category) VALUES($1, $2, $3) RETURNING *;';
+
+      const result = await conn.query(sql, [
+        product.name,
+        product.price,
+        product.category
+      ]);
+      // console.log('user.ts: result', result);
+      conn.release();
+      return result.rows[0];
+      //take supplied user and store in database, on success return user in json form
+    } catch (err) {
+      throw new Error(
+        `Something went wrong, try again. Duplicate product account? Error: ${err}`
+      );
+    }
+  }
+
+  async show(id: string): Promise<Product> {
+    try {
+      // console.log('user.ts: id is ', id);
+      const sql = 'SELECT * FROM products WHERE id=($1);';
+      const conn = await client.connect();
+      // const idNum =
+      const result = await conn.query(sql, [id]);
+      conn.release();
+
+      return result.rows[0];
+    } catch (err) {
+      throw new Error(`There is no product with ID ${id}. Error: ${err}`);
+    }
+  }
+  // async update(id: number, username: string, firstname: string, lastname: string){
+  async update(product: Product): Promise<Product> {
+    try {
+      const sql =
+        'UPDATE products SET name= ($1), price= ($2), category=($3) WHERE products.id = ($4) RETURNING *;';
+      const conn = await client.connect();
+      const result = await conn.query(sql, [
+        product.name,
+        product.price,
+        product.category,
+        product.id
+      ]);
+      conn.release();
+
+      return result.rows[0];
+    } catch (err) {
+      throw new Error(
+        `Something went wrong with updating product with ID=${product.id} and name ${product.name}`
+      );
+    }
+  }
+
+  async delete(id: string): Promise<string> {
+    try {
+      // console.log('user.ts/delete: id is ', id);
+      const feedback = await this.show(id);
+      // console.log('user.ts/delete: feedback ', feedback);
+      const sql = 'DELETE FROM products WHERE id=($1);';
+      const conn = await client.connect();
+
+      const result = await conn.query(sql, [id]);
+      conn.release();
+      // console.log('user.ts/delete: value of result.rows[0] ', result.rows[0]);
+      // return `Success! User with id = ${id} was deleted`
+      return `Success! Prouct with id = ${id} was deleted, Username: ${feedback.name}, (${feedback.price} ${feedback.category})`;
+    } catch (err) {
+      throw new Error(`Cannot delete user with id = ${id}`);
+    }
+  }
+}
