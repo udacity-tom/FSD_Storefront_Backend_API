16abd57c40dedcd6c9861ae7761af1cda3cce488
udacity-tom <udacity@iamtom.com>
2021-09-20 14:16:58 +0200

    Chore: formatting issues/prettier/eslint

--- src/handlers/users.ts
+++ src/handlers/users.ts
@@ -1,114 +1,111 @@
 import express, { Request, Response } from 'express';
-import { User, UserStore } from "../models/user";
-import { AuthStore } from "../middleware/auth";
+import { User, UserStore } from '../models/user';
+import { AuthStore } from '../middleware/auth';
 import checkUserName from '../middleware/checkUserName';
 // import { nextTick } from 'process';
 
-const userStore = new UserStore;
-const auth = new AuthStore;
+const userStore = new UserStore();
+const auth = new AuthStore();
 
-const index = async(req: Request, res: Response) => {
-    try {
-        const users = await userStore.index();
-        res.json(users);
-    } catch (err) {
-        res.status(400).json(err);
-    }
-}
+const index = async (req: Request, res: Response) => {
+  try {
+    const users = await userStore.index();
+    console.log('in users.ts');
+    res.json(users);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+};
 
+const show = async (req: Request, res: Response) => {
+  try {
+    // console.log('users.ts id', req.params.id);
+    const user = await userStore.show(req.params.id);
+    res.json(user);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+};
 
-const show = async(req: Request, res: Response) => {
-    try {
-        // console.log('users.ts id', req.params.id);
-        const user = await userStore.show(req.params.id);
-        res.json(user);
-    } catch (err) {
-        res.status(400).json(err);
-    }
-}
+const create = async (req: Request, res: Response) => {
+  try {
+    const user: User = {
+      username: req.body.username,
+      firstname: req.body.firstname,
+      lastname: req.body.lastname,
+      password: req.body.password
+    };
+    // console.log('users.ts: user value', user);
+    const newUser = await userStore.create(user);
+    // console.log('users.ts: newUser ', newUser);
 
-const create = async(req: Request, res: Response) => {
-    try {
-        const user: User = {
-            username: req.body.username,
-            firstname: req.body.firstname,
-            lastname: req.body.lastname,
-            password_hash: req.body.password
-        }
-        // console.log('users.ts: user value', user);
-        const newUser = await userStore.create(user);
-        // console.log('users.ts: newUser ', newUser);
-        
-        let jwtPayloadData: User = {
-            username: newUser.username,
-            firstname: newUser.firstname,
-            lastname: '',
-            password_hash: ''
-        }        
-        const token = await auth.createToken(jwtPayloadData);
-        // console.log('users.ts: token returned', token);
-        res.send([newUser, token]);
-
-    } catch (err) {
-        res.status(400).json(err);
-    }
-}
+    const jwtPayloadData: User = {
+      username: newUser.username,
+      firstname: newUser.firstname,
+      lastname: '',
+      password: ''
+    };
+    const token = await auth.createToken(jwtPayloadData);
+    // console.log('users.ts: token returned', token);
+    res.send([newUser, token]);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+};
 
 const authenticate = async (req: Request, res: Response) => {
-    try {
-        const {username, password} = req.body;
-        // console.log('users.ts: username', username);
-        const userAuth = await auth.authenticate(username, password);
-        // console.log('users.ts: userAuth', userAuth);
-        // console.log('users.ts: userAuth[1]', userAuth);
-        res.send(userAuth);//returns jwt
-
-    } catch (err) {
-        res.status(400).json(err);
-    }
-}
-
+  try {
+    const { username, password } = req.body;
+    // console.log('users.ts: username', username);
+    const userAuth = await auth.authenticate(username, password);
+    // console.log('users.ts: userAuth', userAuth);
+    // console.log('users.ts: userAuth[1]', userAuth);
+    res.send(userAuth); //returns jwt
+  } catch (err) {
+    res.status(400).json(err);
+  }
+};
 
 const update = async (req: Request, res: Response) => {
-    try {
-        const currentUserDetails = await userStore.show(req.params.id);
-        if(req.body.username){
-            currentUserDetails.username = req.body.username;
-        }
-        if(req.body.firstname){
-            currentUserDetails.firstname = req.body.firstname;
-        }
-        if(req.body.lastname){
-            currentUserDetails.lastname = req.body.lastname;
-        }
-        // console.log('users.ts/update: currentUserDetails', Object.entries( currentUserDetails));
-
-        const updateUser = await userStore.update(currentUserDetails)
-
-        res.status(200).json(updateUser);
-    } catch (err) {
-        res.status(400).json(err);
+  try {
+    const currentUserDetails = await userStore.show(req.params.id);
+    if (req.body.username) {
+      currentUserDetails.username = req.body.username;
     }
-}
+    if (req.body.firstname) {
+      currentUserDetails.firstname = req.body.firstname;
+    }
+    if (req.body.lastname) {
+      currentUserDetails.lastname = req.body.lastname;
+    }
+    // console.log('users.ts/update: currentUserDetails', Object.entries( currentUserDetails));
+
+    const updateUser = await userStore.update(currentUserDetails);
+
+    res.status(200).json(updateUser);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+};
 
 const destroy = async (req: Request, res: Response) => {
-    try {
-        const idToDelete = req.params.id;
-        const userDelete = await userStore.delete(idToDelete);
-        // console.log('users.ts/destroy: userDelete', userDelete);
-        res.json(userDelete);
-    } catch (err) {
-        res.status(400).send(err);
-    }
-}
+  try {
+    const idToDelete = req.params.id;
+    const userDelete = await userStore.delete(idToDelete);
+    // console.log('users.ts/destroy: userDelete', userDelete);
+    res.json(userDelete);
+  } catch (err) {
+    res.status(400).send(err);
+  }
+};
 
 const userRoutes = (app: express.Application) => {
-    app.get('/users', auth.verifyAuthToken, index);
-    app.get('/users/:id', auth.verifyAuthToken, show);
-    app.post('/users/create', auth.verifyAuthToken, checkUserName, create);// == new user
-    app.post('/users/authenticate', authenticate); // == login
-    app.post('/users/update/:id', auth.verifyAuthToken, update);
-    app.delete('/users/delete/:id', auth.verifyAuthToken, destroy);
-}
+  app.get('/users', index);
+  app.get('/users/:id', auth.verifyAuthToken, show);
+  app.post('/users/create', auth.verifyAuthToken, checkUserName, create); // == new user
+  app.post('/users/authenticate', authenticate); // == login
+  app.post('/users/update/:id', auth.verifyAuthToken, update);
+  app.delete('/users/delete/:id', auth.verifyAuthToken, destroy);
+};
 
 export default userRoutes;
--- src/models/user.ts
+++ src/models/user.ts
@@ -1,112 +1,136 @@
-import client from "../database";
-import { AuthStore } from "../middleware/auth";
-import {Request, Response} from 'express';
+import client from '../database';
+import { AuthStore } from '../middleware/auth';
+import { Request, Response } from 'express';
 
-const auth = new AuthStore;
+const auth = new AuthStore();
 
 export type User = {
-    id?: number;
-    username: string;
-    firstname: string;
-    lastname: string;
-    password_hash: string;
+  id?: number;
+  username: string;
+  firstname: string;
+  lastname: string;
+  password: string;
 };
 
 export class UserStore {
-    async index(): Promise<User[]> {
-        
-        try {
-            const sql = 'SELECT * FROM users;';
-            const conn = await client.connect();
+  async index(): Promise<User[]> {
+    try {
+      const sql = 'SELECT * FROM users;';
+      const conn = await client.connect();
 
-            const result = await conn.query(sql);
-            conn.release();
-            return result.rows;
-            
-        } catch (err) {
-            throw new Error(`Could not find any users. Error: ${err}`);
-        }
+      const result = await conn.query(sql);
+      conn.release();
+      return result.rows;
+    } catch (err) {
+      throw new Error(`Could not find any users. Error: ${err}`);
     }
+  }
 
-    async checkUserName(req: Request, res: Response, next: () => void) {
-        try {
-            const sql = 'SELECT * FROM users WHERE username = ($1);';
-            const conn = await client.connect(); 
-            const result = await conn.query(sql, [req.body.username]); 
-            conn.release();
-            console.log('user.ts: result.rows[0]',result.rows[0]);
-            if(result.rows[0] == undefined){
-                next();
-            }
-            res.status(400).json({message: `Something went wrong checking the username. The username isn't unique!`});
-        } catch (err) {
-            throw new Error(`Something went wrong checking username: ${req.body.username}`);
-        }
+  async checkUserName(
+    req: Request,
+    res: Response,
+    next: () => void
+  ): Promise<void> {
+    try {
+      const sql = 'SELECT * FROM users WHERE username = ($1);';
+      const conn = await client.connect();
+      const result = await conn.query(sql, [req.body.username]);
+      conn.release();
+      console.log('user.ts: result.rows[0]', result.rows[0]);
+      if (result.rows[0] == undefined) {
+        next();
+      }
+      res.status(400).json({
+        message: `Something went wrong checking the username. The username isn't unique!`
+      });
+    } catch (err) {
+      throw new Error(
+        `Something went wrong checking username: ${req.body.username}`
+      );
     }
-    
-    async create(user: User): Promise<User> {
-        try {
-            // console.log('user.ts: user ', user);
-            // const existingUser = await this.checkUserName(user.username);
-            user.password_hash = await auth.hashPassword(user.password_hash);
-            // console.log('user.ts: user ', user);
-            const conn = await client.connect();
+  }
 
-            const sql = 'INSERT INTO users (username, firstname, lastname, password_hash) VALUES($1, $2, $3, $4) RETURNING *;';
+  async create(user: User): Promise<User> {
+    try {
+      // console.log('user.ts: user ', user);
+      // const existingUser = await this.checkUserName(user.username);
+      user.password = await auth.hashPassword(user.password);
+      // console.log('user.ts: user ', user);
+      const conn = await client.connect();
 
-            const result = await conn.query(sql, [user.username, user.firstname, user.lastname, user.password_hash]);
-            // console.log('user.ts: result', result);
-            conn.release();
-            return result.rows[0];
-            //take supplied user and store in database, on success return user in json form
-        } catch (err) {
-            throw new Error(`Something went wrong, try again. Duplicate user account? Error: ${err}`);
-        } 
+      const sql =
+        'INSERT INTO users (username, firstname, lastname, password) VALUES($1, $2, $3, $4) RETURNING *;';
+
+      const result = await conn.query(sql, [
+        user.username,
+        user.firstname,
+        user.lastname,
+        user.password
+      ]);
+      // console.log('user.ts: result', result);
+      conn.release();
+      return result.rows[0];
+      //take supplied user and store in database, on success return user in json form
+    } catch (err) {
+      throw new Error(
+        `Something went wrong, try again. Duplicate user account? Error: ${err}`
+      );
     }
+  }
 
-    async show(id: string): Promise<User> {
-        try {
-            // console.log('user.ts: id is ', id);
-            const sql = 'SELECT * FROM users WHERE id=($1);';
-            const conn = await client.connect();
-            // const idNum = 
-            const result = await conn.query(sql, [id]);
-            conn.release();
+  async show(id: string): Promise<User> {
+    try {
+      // console.log('user.ts: id is ', id);
+      const sql = 'SELECT * FROM users WHERE id=($1);';
+      const conn = await client.connect();
+      // const idNum =
+      const result = await conn.query(sql, [id]);
+      conn.release();
 
-            return result.rows[0];
-        } catch (err) {
-            throw new Error(`There is no user with ID ${id}. Error: ${err}`);
-        } 
+      return result.rows[0];
+    } catch (err) {
+      throw new Error(`There is no user with ID ${id}. Error: ${err}`);
     }
-    // async update(id: number, username: string, firstname: string, lastname: string){
-    async update(user: User): Promise<User> {
-        try {
-            const sql = 'UPDATE users SET username= ($1), firstname= ($2), lastname=($3) WHERE users.id = ($4) RETURNING *;';
-            const conn = await client.connect();
-            const result = await conn.query(sql, [user.username, user.firstname, user.lastname, user.id]);
-            conn.release();
+  }
+  // async update(id: number, username: string, firstname: string, lastname: string){
+  async update(user: User): Promise<User> {
+    try {
+      const sql =
+        'UPDATE users SET username= ($1), firstname= ($2), lastname=($3) WHERE users.id = ($4) RETURNING *;';
+      const conn = await client.connect();
+      const result = await conn.query(sql, [
+        user.username,
+        user.firstname,
+        user.lastname,
+        user.id
+      ]);
+      conn.release();
 
-            return result.rows[0];
-        } catch (err) {
-            throw new Error(`Something went wrong with updating user with ID=${user.id} and name ${user.firstname, ' ',user.lastname}`);
-        }
+      return result.rows[0];
+    } catch (err) {
+      throw new Error(
+        `Something went wrong with updating user with ID=${
+          user.id
+        } and name ${(user.firstname, ' ', user.lastname)}`
+      );
     }
+  }
 
-    async delete(id: string): Promise<String> {
-        try {
-            // console.log('user.ts/delete: id is ', id);
-            const feedback = await this.show(id);
-            // console.log('user.ts/delete: feedback ', feedback);
-            const sql = 'DELETE FROM users WHERE id=($1);';
-            const conn = await client.connect();
+  async delete(id: string): Promise<string> {
+    try {
+      // console.log('user.ts/delete: id is ', id);
+      const feedback = await this.show(id);
+      // console.log('user.ts/delete: feedback ', feedback);
+      const sql = 'DELETE FROM users WHERE id=($1);';
+      const conn = await client.connect();
 
-            const result = await conn.query(sql, [id]);
-            conn.release();
-            // console.log('user.ts/delete: value of result.rows[0] ', result.rows[0]);
-            // return `Success! User with id = ${id} was deleted`
-            return `Success! User with id = ${id} was deleted, Username: ${feedback.username}, (${feedback.firstname} ${feedback.lastname})`
-        } catch (err) {
-            throw new Error(`Cannot delete user with id = ${id}`);
-        } 
+      const result = await conn.query(sql, [id]);
+      conn.release();
+      // console.log('user.ts/delete: value of result.rows[0] ', result.rows[0]);
+      // return `Success! User with id = ${id} was deleted`
+      return `Success! User with id = ${id} was deleted, Username: ${feedback.username}, (${feedback.firstname} ${feedback.lastname})`;
+    } catch (err) {
+      throw new Error(`Cannot delete user with id = ${id}`);
     }
+  }
 }
