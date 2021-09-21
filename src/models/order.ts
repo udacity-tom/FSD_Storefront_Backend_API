c40462473762099e2c57dc8c0c57a81772c61719
udacity-tom <udacity@iamtom.com>
2021-09-21 11:27:52 +0200

    Feat: added update/delete

--- src/models/order.ts
+++ src/models/order.ts
@@ -1,8 +1,10 @@
 import client from '../database';
 import { AuthStore } from '../middleware/auth';
 import { Request, Response } from 'express';
+import { User, UserStore } from './user';
 
 const auth = new AuthStore();
+const user = new UserStore();
 
 export type Order = {
   id?: number;
@@ -50,13 +52,32 @@ export class OrderStore {
     }
   }
 
-  //   async update(order: Order): Promise <Order> {
-  //       try {
+  async update(order: Order): Promise<Order> {
+    try {
+      const sql =
+        'Update orders SET user_id= ($1), status= ($2) WHERE users.id= ($3) RETURNING *;';
+      const conn = await client.connect();
+      const result = await conn.query(sql, [order.user_id, order.status]);
+      conn.release();
+      return result.rows[0];
+    } catch (err) {
+      throw new Error(
+        `There was an error with updating the order: ${order.id}`
+      );
+    }
+  }
 
-  //       } catch (err) {
-  //           throw new Error(`There was an error with updating the order: ${id}`)
-  //       }
-  //   }
-
-  //   async delete
+  async delete(id: string): Promise<string> {
+    try {
+      const feedback: Order = await this.show(id);
+      const userDetails: User = await user.show(String(feedback.user_id));
+      const sql = 'DELETE FROM orders WHERE ID=($1);';
+      const conn = await client.connect();
+      const result = await conn.query(sql, [id]);
+      conn.release();
+      return `Success! Your order with id=${id} was deleted. Order ${id} was for ${userDetails.username} with name: ${userDetails.firstname} ${userDetails.lastname}`;
+    } catch (err) {
+      throw new Error(`There was a problem deleting order with id=${id}`);
+    }
+  }
 }
