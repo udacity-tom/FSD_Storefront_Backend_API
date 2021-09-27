99423807a07b94a24c164918493c8e85794c541a
udacity-tom <udacity@iamtom.com>
2021-09-27 14:09:05 +0200

    Feat: added two additional dashboard handlers & services

--- src/handlers/dashboards.ts
+++ src/handlers/dashboards.ts
@@ -5,9 +5,9 @@ import { DashboardQueries } from '../services/dashboard';
 const dashboardQueries = new DashboardQueries();
 
 const topFiveProducts = async (req: Request, res: Response) => {
-  console.log('in topFiveProducts');
+  //   console.log('in topFiveProducts');
   try {
-    console.log('in topFiveProducts');
+    // console.log('in topFiveProducts');
     const products = await dashboardQueries.popularProducts();
     res.json(products);
   } catch (err) {
@@ -15,7 +15,31 @@ const topFiveProducts = async (req: Request, res: Response) => {
   }
 };
 
+const productsByCategory = async (req: Request, res: Response) => {
+  try {
+    const products = await dashboardQueries.productsByCategory(
+      req.params.category
+    );
+    res.json(products);
+  } catch (err) {
+    res.status(400).send(err);
+  }
+};
+
+const userOrderCompleted = async (req: Request, res: Response) => {
+  try {
+    const completeOrders = await dashboardQueries.userOrdersCompleted(
+      req.params.id
+    );
+    res.json(completeOrders);
+  } catch (err) {
+    res.status(400).send(err);
+  }
+};
+
 const dashboardRoutes = (app: express.Application): void => {
   app.get('/products/info/top-5-products', topFiveProducts);
+  app.get('/products/category/:category', productsByCategory);
+  app.get('/users/:id/orders/complete/all', userOrderCompleted);
 };
 export default dashboardRoutes;
--- src/services/dashboard.ts
+++ src/services/dashboard.ts
@@ -1,10 +1,11 @@
 import client from '../database';
-// import { Product } from '../models/product';
+import { Order } from '../models/order';
+import { Product } from '../models/product';
 
 export class DashboardQueries {
   async popularProducts(): Promise<string[]> {
     try {
-      console.log('in dashboard.ts');
+      //   console.log('in dashboard.ts');
       const sql =
         'SELECT product_id, sum(quantity) FROM order_products GROUP BY ROLLUP(product_id) ORDER BY sum(quantity) DESC LIMIT 6;';
       const conn = await client.connect();
@@ -17,6 +18,30 @@ export class DashboardQueries {
       return filtered;
     } catch (err) {
       throw new Error(`Something went wrong!`);
+    }
+  }
+
+  async productsByCategory(category: string): Promise<Product[]> {
+    try {
+      const sql = 'SELECT * FROM products WHERE products.category = ($1);';
+      const conn = await client.connect();
+      const results = await conn.query(sql, [category]);
+      return results.rows;
+    } catch (err) {
+      throw new Error(`Something went wrong! There is no category ${category}`);
+    }
+  }
+  async userOrdersCompleted(uid: string): Promise<Order[]> {
+    try {
+      const sql =
+        "SELECT * FROM orders WHERE user_id= ($1) AND orders.status='complete';";
+      const conn = await client.connect();
+      const results = await conn.query(sql, [uid]);
+      return results.rows;
+    } catch (err) {
+      throw new Error(
+        `Something went wront! No complete orders for user id = ${uid}`
+      );
     }
   }
 }
