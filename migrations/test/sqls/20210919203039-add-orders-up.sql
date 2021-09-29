e9206e43aa5500abb54a49e1307a5af9e1acbfee
udacity-tom <udacity@iamtom.com>
2021-09-29 15:40:01 +0200

    Fix: change layout for readability.

--- migrations/test/sqls/20210919203039-add-orders-up.sql
+++ migrations/test/sqls/20210919203039-add-orders-up.sql
@@ -1,73 +1,9 @@
-INSERT INTO orders (
-    id, 
-    status, 
-    user_id
-    ) VALUES (
-    1, 
-    'complete', 
-    '1'
-    );
-    INSERT INTO orders (
-    id, 
-    status, 
-    user_id
-    ) VALUES (
-    2, 
-    'complete', 
-    '1'
-    );
-    INSERT INTO orders (
-    id, 
-    status, 
-    user_id
-    ) VALUES (
-    3, 
-    'active', 
-    '1'
-    );
-    INSERT INTO orders (
-    id, 
-    status, 
-    user_id
-    ) VALUES (
-    4, 
-    'complete', 
-    '2'
-    );
-    INSERT INTO orders (
-    id, 
-    status, 
-    user_id
-    ) VALUES (
-    5, 
-    'active', 
-    '2'
-    );
-    INSERT INTO orders (
-    id, 
-    status, 
-    user_id
-    ) VALUES (
-    6, 
-    'active', 
-    '5'
-    );
-    INSERT INTO orders (
-    id, 
-    status, 
-    user_id
-    ) VALUES (
-    7, 
-    'complete', 
-    '5'
-    );
-    INSERT INTO orders (
-    id, 
-    status, 
-    user_id
-    ) VALUES (
-    8, 
-    'active', 
-    '5'
-    );
+INSERT INTO orders(id, status, user_id) VALUES(1, 'complete', '1');
+INSERT INTO orders(id, status, user_id) VALUES(2, 'complete', '1');
+INSERT INTO orders(id, status, user_id) VALUES(3, 'active', '1');
+INSERT INTO orders(id, status, user_id) VALUES(4, 'complete','2');
+INSERT INTO orders(id, status, user_id) VALUES(5, 'active', '2');
+INSERT INTO orders(id, status, user_id) VALUES(6, 'active', '5');
+INSERT INTO orders(id, status, user_id) VALUES(7, 'complete', '5');
+INSERT INTO orders(id, status, user_id) VALUES(8, 'active', '5');
 ALTER SEQUENCE orders_id_seq RESTART WITH 9;
