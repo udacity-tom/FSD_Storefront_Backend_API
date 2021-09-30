b32b9cc4eb1502151b0ad646fe993520ca763713
udacity-tom <udacity@iamtom.com>
2021-09-30 16:27:29 +0200

    Chore: tidy up

--- src/database.ts
+++ src/database.ts
@@ -7,15 +7,12 @@ const {
   POSTGRES_HOST,
   POSTGRES_DB,
   POSTGRES_TEST_DB,
-  POSTGRES_USER,
   POSTGRES_USER_DEV,
   POSTGRES_USER_TEST,
-  POSTGRES_PASSWORD,
   POSTGRES_PASSWORD_DEV,
   POSTGRES_PASSWORD_TEST,
   ENV
 } = process.env;
-// console.log(POSTGRES_DB, POSTGRES_HOST, POSTGRES_USER_TEST, POSTGRES_USER_DEV, POSTGRES_TEST_DB, POSTGRES_PASSWORD_DEV,POSTGRES_PASSWORD_TEST, "From database.ts");
 
 let client: Pool = new Pool();
 console.log('current env mode', ENV);
@@ -43,13 +40,4 @@ if (ENV === 'dev') {
   });
 }
 
-// if(ENV === 'setup'){
-//     console.log(`database.ts: Setting up the databases in Postgres`);
-//     client = new Pool({
-//         host: POSTGRES_HOST,
-//         database: POSTGRES_DB,
-//         user: POSTGRES_USER,
-//         password: POSTGRES_PASSWORD
-//     })
-// }
 export default client;
