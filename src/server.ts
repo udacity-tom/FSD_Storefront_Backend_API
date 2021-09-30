84fa1f3cbd41d18a20562c2931c31a8e9de1dbd4
udacity-tom <udacity@iamtom.com>
2021-09-30 16:26:53 +0200

    Feat: added api Endpoints to landing page

--- src/server.ts
+++ src/server.ts
@@ -12,16 +12,18 @@ const app: express.Application = express();
 const address = `${hostAddress}:${portAddress}`;
 
 app.use(bodyParser.json());
-
-app.get('/', function(req: Request, res: Response) {
-  res.send('Hello World!');
-});
-
 //handler routes
 userRoutes(app);
 orderRoutes(app);
 productRoutes(app);
 dashboardRoutes(app);
+
+app.get('/', function(req: Request, res: Response) {
+  res.send(
+    'Storefront Backend API Welcome!<br>' +
+      'GET /users<br> GET /users/:id<br>POST /users/create<br>POST /users/authenticate<br>PUT /users/:id<br>DELETE /users/:id<br>GET /users/:id/orders<br>GET /orders<br>GET /orders/:oid<br>GET /users/:id/orders/:oid<br>POST /users/:id/orders/create<br>POST /users/:id/orders/:oid/add-product<br>DELETE /users/:id/orders/:oid<br>GET /products<br>GET /products/:id<br>POST /products/create<br>PUT /products/:id<br>DELETE /products/:id<br>GET /products/info/top-5-products<br>GET /products/category/:category<br>GET /users/:id/orders/complete/all<br>GET /'
+  );
+});
 
 app.listen(portAddress, function() {
   console.log(`starting app on: ${address}`);
